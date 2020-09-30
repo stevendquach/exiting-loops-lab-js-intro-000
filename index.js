ab6b2b0ade1710333a195fee6931721b320289bf
Steven Quach <stevendquach@gmail.com>
2020-09-30 08:09:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
-    if (array[i] = stopValue ){
+    if (array[i] == stopValue ){
       break
     let array = changeValue
     }}
